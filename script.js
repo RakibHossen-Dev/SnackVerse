ac1cc6e00a9f1714ae12d86a9a47e43a963d1078
Rakib <hellorakibhossen@gmail.com>
2024-10-13 03:09:48 -0700

    menu section done

--- script.js
+++ script.js
@@ -4,3 +4,47 @@ const bersIcon = document.getElementById("ham-bers");
 bersIcon.addEventListener("click", function () {
   mainMenu.classList.toggle("hidden");
 });
+
+const tabs = document.querySelectorAll(".tabs_wraps ul li");
+const all = document.querySelectorAll(".item_wrap");
+const food = document.querySelectorAll(".food");
+const bvarage = document.querySelectorAll(".bvarage");
+const pizza = document.querySelectorAll(".pizza");
+const snack = document.querySelectorAll(".snack");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+
+    const tabval = tab.getAttribute("data-tabs");
+
+    all.forEach((item) => {
+      item.style.display = "none";
+    });
+
+    if (tabval == "food") {
+      food.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabval == "bvarage") {
+      bvarage.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabval == "snack") {
+      snack.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabval == "pizza") {
+      pizza.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabval == "all") {
+      all.forEach((item) => {
+        item.style.display = "block";
+      });
+    }
+  });
+});
